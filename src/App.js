c744b81fa48787405ec8f53711f139f8a3eee593
Ben Yomtov <benyomtov@comcast.net>
2023-02-24 15:03:42 -0500

    added header and created variable color scheme

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container-fluid p-0">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
